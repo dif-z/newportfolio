dd56d048f15b1b5ef1b95aece66e20e5a86f3fbb
dif-z <77906566+dif-z@users.noreply.github.com>
2021-04-29 05:55:53 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,3 +26,41 @@ function navLinkClick() {
     navToggler.click();
   }
 }
+
+//scrollReveal
+const sr = ScrollReveal ({
+    origin: 'top',
+    distance: '60px',
+    duration: 1700,
+    reset: true
+});
+
+
+//reveal banner name and title
+sr.reveal('.banner',{});
+sr.reveal('.nameline',{ delay: 200 });
+sr.reveal('.skill-section-title',{ delay: 300 });
+sr.reveal('.section-header',{});
+sr.reveal('.name',{ delay: 300 });
+sr.reveal('.mail',{ delay: 500 });
+sr.reveal('.msg',{ delay: 700 });
+sr.reveal('.send-button',{ delay: 800 });
+
+//reveal about section
+sr.reveal('.about',{ delay: 100 });
+sr.reveal('.knowmore',{ delay: 400 });
+sr.reveal('.biodata',{ delay: 300 });
+
+//reveal social-media icons
+sr.reveal('.icon-list', { delay: 200 });
+
+// AOS
+AOS.init();
+
+
+document.querySelector('#contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
